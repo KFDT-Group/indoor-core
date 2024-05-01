25e412f6b39331761dfd0d798da2e03cf4829aeb
Konstaphy <Konstaphy@gmail.com>
2024-05-01 18:22:29 +0500

    init: Инициализация солюшна и проекта парсилки двгшек

---
+++ Indoor.DWGParser/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Indoor.DWGParser.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
